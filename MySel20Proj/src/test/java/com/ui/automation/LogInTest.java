37221cc4c88a3e95b60254711e6aaeca9149dd09
Garnepudi <gvpk18@gmail.com>
2019-08-29 14:21:59 +0530

    fixed csvreader code for DDT

--- MySel20Proj/src/test/java/com/ui/automation/LogInTest.java
+++ MySel20Proj/src/test/java/com/ui/automation/LogInTest.java
@@ -1,6 +1,9 @@
 package com.ui.automation;
 
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.testng.ITestContext;
@@ -8,7 +11,7 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
-import com.automation.util.CSVDataProvider;
+import com.automation.util.DataProviderUtil;
 import com.ui.base.BaseUITest;
 import com.ui.pages.LoginPage;
 import com.ui.pages.SecureAreaPage;
@@ -31,12 +34,14 @@ public class LogInTest extends BaseUITest {
 		testConfig.put("testName", testName);
 	}
 
-	 @Test(testName = "Check Login is successful", description = "After login is successful verifies Secure Area page is loaded", priority = 1)
-	 //, groups = 	 "UITestSuite")
+	// @Test(testName = "Check Login is successful", description = "After login is
+	// successful verifies Secure Area page is loaded", priority = 1,
+	// groups="UITestSuite")
 	public void logInTest() {
 		SoftAssert softAssert = new SoftAssert();
 		LoginPage loginPage = new LoginPage(driver, testConfig, testContext, logger);
 		loginPage.login();
+		takeScreenshot("LoginPage");
 		SecureAreaPage securePage = new SecureAreaPage(driver, testConfig, testContext, logger);
 		// Check login is successful
 		softAssert.assertTrue(securePage.loggedInSecureArea(), "Login Failed");
@@ -45,11 +50,22 @@ public class LogInTest extends BaseUITest {
 		softAssert.assertAll();
 	}
 
-	@Test(testName = "Check Login with invalid cred fails", dataProvider = "provideData", dataProviderClass = CSVDataProvider.class, description = "After login is successful verifies Secure Area page is loaded", priority = 1, groups = "UITestSuite")
-	public void negativeLogInTest(HashMap<String, String> testData) {
+	@Test(dataProvider = "csvDataProvider", dataProviderClass = DataProviderUtil.class)
+	// @Test(testName = "Check Login with invalid cred fails", dataProvider =
+	// "provideData", dataProviderClass=DataProviderUtil.class, description = "After
+	// login is successful verifies Secure Area page is loaded", priority = 0,
+	// groups="UITestSuite")
+	public void negativeLogInTest(Map<String, String> testData) {
+
+		for (Map.Entry<String, String> extractedValues : testData.entrySet()) {
+			logger.info("Key Values are" + " " + extractedValues.getKey());
+			logger.info("Extracted Values are" + " " + extractedValues.getValue());
+		}
+
 		String user = testData.get("UserID");
 		String pwd = testData.get("Password");
 		String message = testData.get("Message");
+
 		SoftAssert softAssert = new SoftAssert();
 		LoginPage loginPage = new LoginPage(driver, testConfig, testContext, logger);
 
@@ -57,7 +73,10 @@ public class LogInTest extends BaseUITest {
 		softAssert.assertTrue(loginPage.invaliPwd(user, pwd).contains(message),
 				"Login with invalid pwd is success. Please check and log a bug");
 
+		System.out.println("user Extracted values is" + " " + user);
+
 		softAssert.assertAll();
+
 	}
 
 }
